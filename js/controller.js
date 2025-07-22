fc6466f6e979ab419d7d60eac88ddfd49e5ec3bd
ssergej97 <sergejsubota1997@gmail.com>
2025-07-22 12:42:49 +0300

    hw-37

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,14 @@
 "use strict";
 
 function createController() {
+  // Render category list
+  document.addEventListener("DOMContentLoaded", () => {
+    const categories = dataBase.getData();
+    categories.forEach((category) => {
+      const template = ui.createTemplate(category);
+      ui.renderCategory(template);
+    });
+  });
   // Add a category
   const addBtn = document.querySelector(".add-btn-category");
   addBtn.addEventListener("click", (e) => {
@@ -8,9 +16,34 @@ function createController() {
     const userCategory = prompt("Enter a category:");
     const userData = { userCategory };
     dataBase.setData(userData);
-    console.log(userData);
+    // console.log(userData);
     if (userCategory === null) return null;
-    console.log(userCategory);
+    // console.log(userCategory);
+    const dataCategory = dataBase.getData().at(-1);
+    // console.log(dataCategory);
+    const createCategoryTemplate = ui.createTemplate(dataCategory);
+    ui.renderCategory(createCategoryTemplate);
+  });
+
+  // Add a subcategory
+  const categories = document.querySelector("[data-categories]");
+  categories.addEventListener("click", (e) => {
+    const { target } = e;
+    console.log(target);
+    // console.dir(target);
+    if (target.dataset.subcategory !== "add") return null;
+    const category = target.closest("li");
+    // console.log(category);
+    const categoryId = Number(category.getAttribute("data-category-id"));
+    // console.log(categoryId);
+    const userSubcategory = prompt("Enter a subcategory:");
+    const userData = [userSubcategory];
+    console.log(userData);
+    const dataSubcategory = dataBase.createSubcategory(userData, categoryId);
+    console.log(dataSubcategory);
+    const createSubCategoryTemplate =
+      ui.templateForSubcategory(dataSubcategory);
+    ui.renderSubcategory(createSubCategoryTemplate);
   });
 }
 
--- js/view.js
+++ js/view.js
@@ -2,12 +2,23 @@
 
 function createUi() {
   // Create template
-  const createTemplate = ({ category, subcategories, id }) => {
+  const createTemplate = ({ userCategory, id }) => {
     const wrapper = document.createElement("li");
     wrapper.setAttribute("data-category-id", id);
-    const categoryName = document.createElement("h2");
-    categoryName.innerHTML = `${category}`;
-    wrapper.append(categoryName);
+    const content = `
+        <h2>${userCategory}</h2>
+        <ul data-subcategory>
+<!--            <li>Washing machines</li>-->
+<!--            <li>Dishwashers</li>-->
+<!--            <li>Fridges</li>-->
+        </ul>
+        <button class="add-btn-subcategory" data-subcategory="add">Add a subcategory</button>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn">Delete</button>
+    `.trim();
+
+    wrapper.innerHTML = content;
+    // console.log(wrapper);
 
     return wrapper;
   };
@@ -16,6 +27,24 @@ function createUi() {
   const renderCategory = (template) => {
     const categories = document.querySelector("[data-categories]");
     categories.append(template);
+  };
+
+  const templateForSubcategory = (data) => {
+    const subcategory = document.createElement("li");
+    subcategory.innerHTML = `${data}`;
+    return subcategory;
+  };
+
+  const renderSubcategory = (template) => {
+    const subCategories = document.querySelector("[data-subcategory]");
+    subCategories.append(template);
+  };
+
+  return {
+    createTemplate,
+    renderCategory,
+    templateForSubcategory,
+    renderSubcategory,
   };
 }
 
