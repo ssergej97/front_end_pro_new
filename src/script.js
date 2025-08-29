f60305dbcf684e8444c7ceeb5eb7ba0d7eaee5a8
ssergej97 <sergejsubota1997@gmail.com>
2025-08-29 13:09:22 +0300

    hw-47

--- src/script.js
+++ src/script.js
@@ -36,6 +36,9 @@ class Todo {
     }
     this.render()
     this.bindEvents()
+    this.calcInProgressStatusTasks()
+    this.renderInProgressCounter()
+    this.renderDoneTasksCounter()
   }
 
   getItemsFromLocalStorage() {
@@ -59,6 +62,57 @@ class Todo {
       this.localStorageKey,
       JSON.stringify(this.state.items)
     )
+  }
+
+  calcInProgressStatusTasks() {
+    const currentData = this.state.items;
+    let numberOfInProgressTasks = 0;
+    currentData.forEach((task) => {
+      if (task.status === 'in progress') {
+        numberOfInProgressTasks += 1;
+      }
+    })
+    return numberOfInProgressTasks;
+  }
+
+  renderInProgressCounter() {
+    this.inProgressTasksElement.innerText = `${this.calcInProgressStatusTasks()}`;
+  }
+
+  changeStatusTask(id) {
+    this.state.items = this.state.items.map((item) => {
+      if (item.id === id && item.status === 'in progress') {
+        return {
+          ...item,
+          status: 'done',
+        }
+      } else if (item.id === id && item.status === 'done') {
+        return {
+          ...item,
+          status: 'in progress',
+        }
+      }
+
+      return item
+    })
+    this.saveItemsToLocalStorage()
+    this.renderDoneTasksCounter()
+    this.renderInProgressCounter()
+  }
+
+  calcDoneStatusTasks() {
+    const currentData = this.state.items;
+    let numberOfDoneTasks = 0;
+    currentData.forEach((task) => {
+      if (task.status === 'done') {
+        numberOfDoneTasks += 1;
+      }
+    })
+    return numberOfDoneTasks;
+  }
+
+  renderDoneTasksCounter() {
+    this.doneTasksElement.innerText = `${this.calcDoneStatusTasks()}`;
   }
 
   render() {
@@ -109,7 +163,7 @@ class Todo {
           />
         </svg>
       </button>
-    </li>  
+    </li>
     `).join('')
 
     const isEmptyItems = this.state.items.length === 0
@@ -122,9 +176,11 @@ class Todo {
       id: crypto?.randomUUID() ?? Date.now().toString(),
       title,
       isChecked: false,
+      status: 'in progress',
     })
     this.saveItemsToLocalStorage()
     this.render()
+    this.renderInProgressCounter()
   }
 
   deleteItem(id) {
@@ -162,6 +218,7 @@ class Todo {
 
   onClick = ({ target }) => {
     if (target.matches(this.selectors.itemDeleteButton)) {
+      console.log('test');
       const itemElement = target.closest(this.selectors.item)
       const itemCheckBoxElement = itemElement.querySelector(this.selectors.itemCheckbox)
 
@@ -169,6 +226,8 @@ class Todo {
 
       setTimeout(() => {
         this.deleteItem(itemCheckBoxElement.id)
+        this.renderInProgressCounter()
+        this.renderDoneTasksCounter()
       }, 400)
     }
   }
@@ -176,13 +235,148 @@ class Todo {
   onChange = ({ target }) => {
     if (target.matches(this.selectors.itemCheckbox)) {
       this.toggleCheckedState(target.id)
+      this.changeStatusTask(target.id)
     }
+  }
+
+  filterOnChange = ( {target} ) => {
+    console.dir(target);
+    if (target.dataset.jsFilterTaskInput === '') {
+      console.log('test');
+      const options = Array.from(target.children);
+      options.forEach((option) => {
+        if (option.value === 'all') {
+          // this.filterAllTasks();
+        } else if (option.value === 'active') {
+          this.filterActiveTasks();
+        } else if (option.value === 'completed') {
+          this.filterCompletedTasks();
+        }
+      })
+    }
+  }
+
+  filterAllTasks() {
+    this.render();
+  }
+
+  filterActiveTasks() {
+    const currentItems = this.state.items;
+    const inProgressItems = [];
+
+    currentItems.forEach((item) => {
+      if (item.status === 'in progress') {
+        inProgressItems.push(item);
+      }
+    })
+
+    this.listElement.innerHTML = inProgressItems.map(({ id, title, isChecked }) => `
+      <li
+      class="todo__item todo-item"
+      data-js-todo-item
+    >
+      <input
+        type="checkbox"
+        id="${id}"
+        class="todo-item__checkbox"
+        ${isChecked ? 'checked' : ''}
+        data-js-todo-item-checkbox
+      >
+      <label
+        for="${id}"
+        class="todo-item__label"
+        data-js-todo-item-label
+      >
+        ${title}
+      </label>
+      <button
+        class="todo-item__delete-button"
+        type="button"
+        title="Delete"
+        aria-label="delete"
+        data-js-todo-item-delete-button
+      >
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M15 5L5 15M5 5L15 15"
+            stroke="#757575"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </li>
+    `).join('')
+  }
+
+  filterCompletedTasks () {
+    const currentItems = this.state.items;
+    const completedItems = [];
+
+    currentItems.forEach((item) => {
+      if (item.status === 'done') {
+        completedItems.push(item);
+      }
+    })
+
+    this.listElement.innerHTML = completedItems.map(({ id, title, isChecked }) => `
+      <li
+      class="todo__item todo-item"
+      data-js-todo-item
+    >
+      <input
+        type="checkbox"
+        id="${id}"
+        class="todo-item__checkbox"
+        ${isChecked ? 'checked' : ''}
+        data-js-todo-item-checkbox
+      >
+      <label
+        for="${id}"
+        class="todo-item__label"
+        data-js-todo-item-label
+      >
+        ${title}
+      </label>
+      <button
+        class="todo-item__delete-button"
+        type="button"
+        title="Delete"
+        aria-label="delete"
+        data-js-todo-item-delete-button
+      >
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M15 5L5 15M5 5L15 15"
+            stroke="#757575"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </li>
+    `).join('')
   }
 
   bindEvents() {
     this.newTaskFormElement.addEventListener('submit', this.onNewTaskFormSubmit)
     this.listElement.addEventListener('click', this.onClick)
     this.listElement.addEventListener('change', this.onChange)
+    this.filterTaskFormElement.addEventListener('change', this.filterOnChange)
   }
 }
 
