230fc74396c15c443229410acb8d99f17822c3cb
ssergej97 <sergejsubota1997@gmail.com>
2025-09-02 20:18:45 +0300

    hw-50

--- src/script.js
+++ src/script.js
@@ -1,29 +1,52 @@
 'use strict';
 
 function fetchUserData(userId) {
-  const url = 'https://jsonplaceholder.typicode.com/users';
+  const url = `https://jsonplaceholder.typicode.com/users/${userId}`;
   return fetch(url);
 }
 
-fetchUserData().then((respond) => {
-  return respond.json()
-})
-
 function getUsersData(userIds) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-
-    }, 1000)
-  })
+  const url = `https://jsonplaceholder.typicode.com/users`;
+  return fetch(url);
 }
-
-Promise.allSettled([getUsersData(userIds)])
 
 const userIds = [1, 2, 3, 4, 5];
 
-getUsersData(userIds).then((result) => {
-  console.log("✅ Success:", result.success);
-  console.log("❌ Errors:", result.errors);
-})
+getUsersData(userIds)
+  .then((data) => {
+    let arrOfUsers = [];
+    userIds.forEach((id) => {
+      arrOfUsers.push(fetchUserData(id));
+      // for (let i = 0; i < data.length; i++) {
+      //   if (id === data[i].id) {
+      //     users.push(data[i])
+      //   }
+      // }
+    })
+    // console.log(users);
+    return arrOfUsers;
+  })
+  .then((users) => {
+    const currentUsers = [];
+    console.log(users);
+    users.forEach((user) => {
+      console.log(user.json());
+      currentUsers.push(user.json())
+    })
+    console.log(currentUsers);
+    return currentUsers;
+  })
+// function getUsersData(userIds) {
+//   return new Promise((resolve, reject) => {
+//
+//   })
+// }
+//
+// const userIds = [1, 2, 3, 4, 5];
+//
+// getUsersData(userIds).then((result) => {
+//   console.log("✅ Success:", result.success);
+//   console.log("❌ Errors:", result.errors);
+// })
 
 
