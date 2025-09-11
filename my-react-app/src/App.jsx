182523adecf1b7b3f12522330d1e9add76aa5054
ssergej97 <sergejsubota1997@gmail.com>
2025-09-11 18:06:48 +0300

    hw-54

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,65 +1,145 @@
-function App() {
+import { useState } from 'react';
 
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a
-            className="navbar-brand"
-            href="#"
-          >Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse"
-            id="navbarNav"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href="#"
-                >Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  aria-disabled="true"
-                >Disabled
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  )
+// function MyButton() {
+//   return (
+//     <button>
+//       I'm a button
+//     </button>
+//   );
+// }
+//
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Welcome to my app</h1>
+//       <MyButton />
+//     </div>
+//   );
+// }
+
+// const user = {
+//   name: 'Hedy Lamarr',
+//   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+//   imageSize: 90,
+// };
+//
+// export default function Profile() {
+//   return (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img
+//         className="avatar"
+//         src={user.imageUrl}
+//         alt={'Photo of ' + user.name}
+//         style={{
+//           width: user.imageSize,
+//           height: user.imageSize
+//         }}
+//       />
+//     </>
+//   );
+// }
+
+// const products = [
+//   { title: 'Cabbage', isFruit: false, id: 1 },
+//   { title: 'Garlic', isFruit: false, id: 2 },
+//   { title: 'Apple', isFruit: true, id: 3 },
+// ];
+//
+// export default function ShoppingList() {
+//   const listItems = products.map(product =>
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? 'magenta' : 'darkgreen'
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   );
+//
+//   return (
+//     <ul>{listItems}</ul>
+//   );
+// }
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Counters that update separately</h1>
+//       <MyButton />
+//       <MyButton />
+//     </div>
+//   );
+// }
+//
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+//
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+//
+//   return (
+//     <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
+
+// export default function MyApp() {
+//   const [count, setCount] = useState(0);
+//
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+//
+//   return (
+//     <div>
+//       <h1>Counters that update together</h1>
+//       <MyButton count={count} onClick={handleClick} />
+//       <MyButton count={count} onClick={handleClick} />
+//     </div>
+//   );
+// }
+//
+// function MyButton({ count, onClick }) {
+//   return (
+//     <button onClick={onClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return <button className="square" onClick={handleClick}>{value}</button>;
 }
 
-export default App
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+    )
+
+}
