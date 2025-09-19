78eb1f4ef920e51574692ea40f70643f2425f82f
ssergej97 <sergejsubota1997@gmail.com>
2025-09-19 19:21:40 +0300

    hw-57

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from "./components/Header.jsx";
 import Aside from "./components/Aside.jsx";
-import Container from "./components/Container.jsx";
 
 class App extends React.Component {
   render() {
@@ -9,9 +8,7 @@ class App extends React.Component {
       <>
         <Header></Header>
         <main>
-          <Container>
             <Aside></Aside>
-          </Container>
         </main>
       </>
     )
