9e1187889c44f5bf7b864642bfc6fefba37d7b75
ssergej97 <sergejsubota1997@gmail.com>
2025-10-02 18:04:36 +0300

    hw-60

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Timer from "./components/Timer";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: true
+    }
+  }
 
   render() {
     return (
       <>
-        <Timer></Timer>
+        {
+          this.state.status && <Timer></Timer>
+        }
+        <Button onClick={() => this.setState({status: false})} className="m-2" variant="danger">Unmount Timer</Button>
       </>
     );
   }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,17 +1,37 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: new Date().getSeconds()
+      count: 0
     }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.setState((prevState) => ({count: prevState.count + 1}))
+    }, 1000)
+  }
+
+  componentDidUpdate(prevProps,prevState, snapshot) {
+    if (prevState.count !== this.state.count) {
+      console.log(this.state.count);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
   }
 
   render() {
     return (
       <>
-        <h1>{this.state.data}</h1>
+        <h1 className="m-2">{this.state.count}</h1>
+        <Button className="m-2" variant="primary">Start</Button>
+        <Button className="m-2" variant="secondary">Stop</Button>
+        <Button className="m-2" variant="success">Reset</Button>
       </>
     );
   }
