833fdbe2d2241317035b0d559cceb2b692ace618
ssergej97 <sergejsubota1997@gmail.com>
2025-10-03 13:19:20 +0300

    hw-60

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import React from "react";
 import Timer from "./components/Timer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: true
-    }
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       status: true
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <>
+//         {
+//           this.state.status && <Timer></Timer>
+//         }
+//         <Button onClick={() => this.setState({status: false})} className="m-2" variant="danger">Unmount Timer</Button>
+//       </>
+//     );
+//   }
+// }
 
-  render() {
-    return (
-      <>
-        {
-          this.state.status && <Timer></Timer>
-        }
-        <Button onClick={() => this.setState({status: false})} className="m-2" variant="danger">Unmount Timer</Button>
-      </>
-    );
-  }
+const App = () => {
+  const [status, setStatus] = useState(true)
+
+  return (
+    <>
+      {status && <Timer></Timer>}
+      <Button onClick={() => setStatus(false)} className="m-2" variant="danger">Unmount Timer</Button>
+    </>
+  )
 }
 
 export default App
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,40 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    }
-  }
+// Class component lifecycle
+// class Timer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     }
+//   }
+//
+//   componentDidMount() {
+//     this.timerID = setInterval(() => {
+//       this.setState((prevState) => ({count: prevState.count + 1}))
+//     }, 1000)
+//   }
+//
+//   componentDidUpdate(prevProps,prevState, snapshot) {
+//     if (prevState.count !== this.state.count) {
+//       console.log(this.state.count);
+//     }
+//   }
+//
+//   componentWillUnmount() {
+//     clearInterval(this.timerID)
+//   }
+//
+//   render() {
+//     return (
+//       <>
+//         <h1 className="m-2">{this.state.count}</h1>
+//         <Button className="m-2" variant="primary">Start</Button>
+//         <Button className="m-2" variant="secondary">Stop</Button>
+//         <Button className="m-2" variant="success">Reset</Button>
+//       </>
+//     );
+//   }
+// }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => {
-      this.setState((prevState) => ({count: prevState.count + 1}))
-    }, 1000)
-  }
 
-  componentDidUpdate(prevProps,prevState, snapshot) {
-    if (prevState.count !== this.state.count) {
-      console.log(this.state.count);
-    }
-  }
+// Functional component lifecycle
+const Timer = () => {
+  const [count, setCount] = useState(0);
 
-  componentWillUnmount() {
-    clearInterval(this.timerID)
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
 
-  render() {
-    return (
-      <>
-        <h1 className="m-2">{this.state.count}</h1>
-        <Button className="m-2" variant="primary">Start</Button>
-        <Button className="m-2" variant="secondary">Stop</Button>
-        <Button className="m-2" variant="success">Reset</Button>
-      </>
-    );
-  }
+    console.log(count);
+
+    return () => clearInterval(interval);
+  })
+
+  return (
+    <>
+      <h1 className="m-2">{count}</h1>
+      <Button className="m-2" variant="primary">Start</Button>
+      <Button className="m-2" variant="secondary">Stop</Button>
+      <Button className="m-2" variant="success">Reset</Button>
+    </>
+  )
 }
 
 export default Timer
